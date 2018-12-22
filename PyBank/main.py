4fc31a6b25da2ffb783670a1fcd927ee95967104
Elon Phoenix <carolinemiller@fastmail.fm>
2018-12-22 11:49:10 -0500

    finished!

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,51 +1,55 @@
 #Bank 
-
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
-#The total number of months included in the dataset
-#The net total amount of "Profit/Losses" over the entire period
-#The average of the changes in "Profit/Losses" over the entire period
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
-#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+#by Caroline Miller
 
 #first import the csv file into the environment
 import csv
-import pandas as pd
-import math
-import datetime
 
-
-with open('budget_data.csv', newline = "") as csvfile:
+date = []
+profit = []
+change_profit = []
+with open('budget_data2.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    #remove the headers
+    next(csvreader, None)
+
     for row in csvreader:
 
-        Date = row[0]
+        date.append(row[0])
+        
+        profit.append(int(row[1]))
 
-        Profit = row[1]
+    #then, find a way to sum the time elapsed in months for the date column and store it as a variable to print later
+    mymonth = set(date)
+    total_months = len(mymonth)
 
-#then, find a way to sum the time elapsed in months for the date column and store it as a variable to print later
-        #datetime stuff
-#for loop that adds up each value in the profit column w a counter
-        #rowcoutner = 0
-        #print(sum_profit)
-#build an average function
-#avg function just like above, again stored as a 
-        #avg_profit = sum_profit/len(Profit)
-        #print(avg_profit)
-#find max for profits, store as var to print later
-        #max_profit = max(Profit)
-        #print(max_profit)
-#find min for profits, store as var to print later
-        #min_profit = min(Profit)
-        #print(min_profit)
+    #loop that adds up each value in the profit column w a counter
+    sum_profit = sum(profit)
+
+    #sum of the change of profit divided by length of change of profit
+    for i in range(0, len(profit) -1):
+        change_profit.append(int(profit[i+1]) - int(profit[i]))
+    avg_profit_change = sum(change_profit)/len(change_profit)
+    #round average to two decimal places
+    avg_profit_change = round(avg_profit_change, 2)
+
+    #find max for CHANGE IN REVENUE, store as var to print later
+    max_profit = max(change_profit)
+    max_date = date[change_profit.index(max_profit) + 1]
+    print(max_date)
+    #find min for change of profits, store as var to print later
+    min_profit = min(change_profit)
+    min_date = date[change_profit.index(min_profit) + 1]
+
 #print every single variable with the format asked (below)
-  #Financial Analysis
-  #print('----------------------------')
-  # print('Total Months: ' + months)
-  # print('Total: ' + sum_profit)
-  # print('Average  Change: ' + avg_profit)
-  # print('Greatest Increase in Profits: ' + max_profit)
-  # print('Greatest Decrease in Profits: ' + min_profit)
 
+print("Financial Analysis")
+print('----------------------------')
+print('Total Months: ' + str(total_months))
+print('Total: $' + str(sum_profit))
+print('Average  Change: $' + str(avg_profit_change))
+print('Greatest Increase in Profits: ' + max_date + ' $' +str(max_profit))
+print('Greatest Decrease in Profits: ' + min_date + ' $' + str(min_profit))
+
+#put path
 #write file as a text
-# to set directory: cd Documents/Github/PythonHomework/PyBank
+#to set directory: cd Documents/Github/PythonHomework/PyBank python main.py
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,83 @@
 #Poll
+#by Caroline Miller
 
 #Your task is to create a Python script that analyzes the votes and calculates each of the following:
 #The total number of votes cast
 #A complete list of candidates who received votes
-#The percentage of votes each candidate won
 #The total number of votes each candidate won
+#The percentage of votes each candidate won
 #The winner of the election based on popular vote.
 #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
 #first import the csv into the environment
+voter_id = []
+county = []
+candidate = []
+
+import csv
+with open('election_data.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    #remove the headers
+    next(csvreader, None)
+
+    for row in csvreader:
+
+    #convert columns into lists so they can be manipulated
+        voter_id.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+    
+    #find total number of votes by counting how long the voter id column is, store as var to print later
+    total_votes = len(voter_id)
+
+    #get unique values of candidates with set function, store as var to print later
+    candidate_names = set(candidate)
+
+    #find out volume of votes for each candidate
+    khan_volume = candidate.count("Khan")
+    correy_volume = candidate.count("Correy")
+    li_volume = candidate.count("Li")
+    otooley_volume = candidate.count("O'Tooley")
+
+    #percentage of votes each candidate won
+     #round percentage so there's three decimal places
+    k_percentage = (khan_volume/len(candidate)) * 100
+    khan_percentage = round(k_percentage, 3)
+
+    c_percentage = (correy_volume/len(candidate)) * 100
+    correy_percentage = round(c_percentage, 3)
+
+    l_percentage = (li_volume/len(candidate)) * 100
+    li_percentage = round(l_percentage, 3)
+
+    o_percentage = (otooley_volume/len(candidate)) * 100
+    otooley_percentage = round(o_percentage, 3)
+
+    #find winner of election by making list of all vote counts then running an if statement
+    candidate_votes = [khan_volume, correy_volume, li_volume, otooley_volume]
+
+    if max(candidate_votes) == khan_volume:
+        winner = "Khan"
+    elif max(candidate_votes) == correy_volume:
+        winner = "Correy"
+    elif max(candidate_votes) == li_volume:
+        winner = "Li"
+    else:
+        winner = "O'Tooley"
+
+
+    #print results
+
+    print('Election Results')
+    print('-------------------------')
+    print('Total Votes: ' + str(total_votes))
+    print('-------------------------')
+    print('Khan: ' + str(khan_percentage) +  '%' + ' (' + str(khan_volume) + ')')
+    print('Correy: ' + str(correy_percentage) +  '%' + ' (' + str(correy_volume) + ')')
+    print('Li: ' + str(li_percentage) + '%' + ' (' + str(li_volume) + ')')
+    print("O'Tooley: " + str(otooley_percentage) + '%' + ' (' + str(otooley_volume) + ')')
+    print('-------------------------')
+    print('Winner: ' + winner)
+
+    #copy path
+    #export as text file
